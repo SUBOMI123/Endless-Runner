c14059a68200697b99e8f6a5dc05eecc5cc7e97a
SUBOMI123 <79486152+SUBOMI123@users.noreply.github.com>
2021-08-29 15:13:22 -0400

    Add files via upload

---
+++ playerboundary.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class playerboundary :MonoBehaviour
+{
+
+	public float playerBoundaryLeft;
+	public float playerBoundaryRight; 
+
+
+
+
+    private void Update()
+	{
+
+
+
+		if (transform.position.x < playerBoundaryLeft)
+		{
+			transform.position = new Vector3(playerBoundaryLeft, transform.position.y, transform.position.z);
+		}
+
+
+	}
+
+}
---
+++ playermove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermove : MonoBehaviour {
+
+
+	public KeyCode moveL;
+	public KeyCode moveR;
+
+
+	public float horizvel = 0;
+
+	public float vertvel = 0;
+
+	private int currentLane = 2;
+
+	// Use this for initialization
+	private void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+
+
+		GetComponent<Rigidbody> ().velocity = new Vector3 (horizvel, 0, 1);
+
+		if (Input.GetKeyDown (moveL) &&(currentLane > 1)) {
+			horizvel = -2;
+			StartCoroutine(stopSlide());
+			currentLane -= 1;
+		}
+
+		if (Input.GetKeyDown (moveR) && (currentLane < 3)) {
+			horizvel = 2;
+			StartCoroutine(stopSlide());
+			currentLane += 1;
+		}
+
+
+	}
+	IEnumerator stopSlide() 
+	{
+		yield return new WaitForSeconds (.5f);
+		horizvel = 0;
+	}
+}
